e4656695dc3cc2f02744fcee526467d6c098616e
Thales-github <thalesvitoroliveiracirino@gmail.com>
2022-07-28 19:20:07 -0300

    Atualização do script de busca

--- index.php
+++ index.php
@@ -1,22 +1,25 @@
 <?php
 
+function removeCaracteresNaoNumericos($valor)
+{
+    return preg_replace("/[^0-9]/", "", $valor);
+}
 
-    // create curl resource
+function realizaBusca($cep)
+{
     $curl = curl_init();
-
-    // set url
-    curl_setopt($curl, CURLOPT_URL, "https://viacep.com.br/ws/02990010/json/");
-
-    //return the transfer as a string
+    curl_setopt($curl, CURLOPT_URL, "https://viacep.com.br/ws/$cep/json/");
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-    // $output contains the output string
     $output = curl_exec($curl);
-
-    var_dump(json_decode($output));
-
-    // close curl resource to free up system resources
-    curl_close($curl);     
-
-    
-?>
+    curl_close($curl); 
+    return json_decode($output);
+     
+}
+$cep = "02990010";
+$cep = removeCaracteresNaoNumericos($cep);
+$resultado = realizaBusca($cep);
+if ($resultado == null) {
+   echo "Erro ao buscar este CEP";
+}else {
+    var_dump($resultado);
+}
