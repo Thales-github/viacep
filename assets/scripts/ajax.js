401b79cced2a811d51ca2c965468492219882ef4
Thales-github <thalesvitoroliveiracirino@gmail.com>
2022-08-15 20:38:26 -0300

    adição de mensagem de erro e limpeza de campos em seguida

--- assets/scripts/ajax.js
+++ assets/scripts/ajax.js
@@ -8,20 +8,19 @@ campoCep.addEventListener("blur", () => {
     ajax.open("GET", `https://viacep.com.br/ws/${cep}/json/`);
 
     ajax.onload = function () {
+        
         let dadosEndereco = JSON.parse(ajax.responseText);
-        //let resultado = ajax.responseText;
 
         if (!dadosEndereco.logradouro) {
-            alert("Cep inválido!");
+            document.querySelector("#frmCep").reset();
+            swal("Erro!", "O Cep digitado é Inválido!", "error");
             return;
         }
-        console.log(dadosEndereco);
+        
         document.querySelector("#txtEndereco").value = dadosEndereco.logradouro;
         document.querySelector("#txtBairro").value = dadosEndereco.bairro;
         document.querySelector("#txtCidade").value = dadosEndereco.localidade;
         document.querySelector("#txtUf").value = dadosEndereco.uf;
-
-
     }
     ajax.send();
 });
